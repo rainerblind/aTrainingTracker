2a9c55bb2b7c4a42c82ed80bb998e3d173ba2642
Rainer Blind <rainer.blind@gmail.com>
2026-01-12 21:55:55 +0100

    Bugfix/Feature: Export files and upload via worker: Migrate ExportInfo from Java to Kotlin.

--- app/src/main/java/com/atrainingtracker/trainingtracker/exporter/ExportInfo.kt
+++ app/src/main/java/com/atrainingtracker/trainingtracker/exporter/ExportInfo.kt
@@ -1,6 +1,6 @@
 /*
  * aTrainingTracker (ANT+ BTLE)
- * Copyright (C) 2011 - 2019 Rainer Blind <rainer.blind@gmail.com>
+ * Copyright (C) 2011 -2026 Rainer Blind <rainer.blind@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,47 +16,68 @@
  * along with this program.  If not, see https://www.gnu.org/licenses/gpl-3.0
  */
 
-package com.atrainingtracker.trainingtracker.exporter;
+package com.atrainingtracker.trainingtracker.exporter
 
-import androidx.annotation.NonNull;
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.File
 
-import java.io.File;
+/**
+ * A data class holding all necessary information for an export operation.
+ * Can be serialized to and from JSON to be easily passed to background workers.
+ */
+data class ExportInfo(
+    val fileBaseName: String,
+    val fileFormat: FileFormat,
+    val exportType: ExportType
+) {
 
-public class ExportInfo {
-    protected final String mFileBaseName;
-    protected final FileFormat mFileFormat;
-    protected final ExportType mExportType;
+    val shortPath: String
+        get() = fileFormat.dirName + File.separator + fileName
 
-    public ExportInfo(String fileBaseName, FileFormat fileFormat, ExportType exportType) {
-        mFileBaseName = fileBaseName;
-        mFileFormat = fileFormat;
-        mExportType = exportType;
+    val fileName: String
+        get() = fileBaseName + fileFormat.fileEnding
+
+    override fun toString(): String {
+        return "$exportType: $fileFormat: $fileBaseName"
     }
 
-    public String getFileBaseName() {
-        return mFileBaseName;
+    /**
+     * Serializes the ExportInfo object to a JSON string.
+     * @return A JSON representation of the object.
+     * @throws JSONException if there is an error creating the JSON object.
+     */
+    @Throws(JSONException::class)
+    fun toJson(): String {
+        return JSONObject().apply {
+            put(KEY_FILE_BASE_NAME, fileBaseName)
+            put(KEY_FILE_FORMAT, fileFormat.name)
+            put(KEY_EXPORT_TYPE, exportType.name)
+        }.toString()
     }
 
-    @NonNull
-    public String getShortPath() {
-        return mFileFormat.getDirName() + File.separator + getFileName();
-    }
+    companion object {
+        // Keys for JSON serialization/deserialization
+        private const val KEY_FILE_BASE_NAME = "fileBaseName"
+        private const val KEY_FILE_FORMAT = "fileFormat"
+        private const val KEY_EXPORT_TYPE = "exportType"
 
-    @NonNull
-    public String getFileName() {
-        return mFileBaseName + mFileFormat.getFileEnding();
-    }
+        /**
+         * Deserializes a JSON string into an ExportInfo object.
+         * @param json The JSON string to parse.
+         * @return A new ExportInfo object.
+         * @throws JSONException if the JSON is invalid or missing keys.
+         */
+        @JvmStatic
+        @Throws(JSONException::class)
+        fun fromJson(json: String): ExportInfo {
+            val jsonObject = JSONObject(json)
+            val fileBaseName = jsonObject.getString(KEY_FILE_BASE_NAME)
+            // Use valueOf() to safely convert string back to enum
+            val fileFormat = FileFormat.valueOf(jsonObject.getString(KEY_FILE_FORMAT))
+            val exportType = ExportType.valueOf(jsonObject.getString(KEY_EXPORT_TYPE))
 
-    public FileFormat getFileFormat() {
-        return mFileFormat;
-    }
-
-    public ExportType getExportType() {
-        return mExportType;
-    }
-
-    @NonNull
-    public String toString() {
-        return mExportType.name() + ": " + mFileFormat.name() + ": " + mFileBaseName;
+            return ExportInfo(fileBaseName, fileFormat, exportType)
+        }
     }
 }
