e9c0b845536940207c236d3bed472c704024711b
Rainer Blind <rainer.blind@gmail.com>
2026-01-20 06:34:23 +0100

    Cleanup: Delete no longer used files.

--- app/src/main/java/com/atrainingtracker/trainingtracker/exporter/IExportProgressListener.java
+++
@@ -1,9 +0,0 @@
-package com.atrainingtracker.trainingtracker.exporter;
-
-/**
- * A simple listener to report progress from a long-running export process
- * to the calling component (e.g., a Worker).
- */
-public interface IExportProgressListener {
-    void onProgress(int max, int current);
-}
