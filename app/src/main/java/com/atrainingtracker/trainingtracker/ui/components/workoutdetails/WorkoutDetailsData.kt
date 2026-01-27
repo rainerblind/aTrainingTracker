b5ae89b36ea3c8fb49cc6dfe6f2a7f18fbe40a2f
Rainer Blind <rainer.blind@gmail.com>
2026-01-27 07:02:11 +0100

    Refactor WorkoutDetailsViewHolder: Rewriting the WorkoutDetailsViewHolder in Kotlin and introducing the WorkoutDetailsData.kt data class.

---
+++ app/src/main/java/com/atrainingtracker/trainingtracker/ui/components/workoutdetails/WorkoutDetailsData.kt
@@ -0,0 +1,24 @@
+package com.atrainingtracker.trainingtracker.ui.components.workoutdetails
+
+
+import com.atrainingtracker.banalservice.BSportType
+
+/**
+ * A simple Data Transfer Object (DTO) to hold all the necessary information
+ * for the WorkoutDetailsViewHolder.
+ *
+ * Using a 'data class' provides component functions, equals(), hashCode(), and toString() automatically.
+ */
+data class WorkoutDetailsData(
+    val totalDistance: Double,
+    val activeTimeSec: Int,
+    val totalTimeSec: Int,
+    val avgSpeedMps: Float,
+    val ascentMeters: Int,
+    val descentMeters: Int,
+    val sportType: BSportType,
+
+    val maxDisplacement: Double?,
+    val minAltitude: Double?,
+    val maxAltitude: Double?
+)
