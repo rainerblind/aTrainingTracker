e3351775e22f328e7eeb9a610534fbafe8561653
Rainer Blind <rainer.blind@gmail.com>
2026-02-15 00:45:07 +0100

    Chore: Remove no longer needed imports.

--- app/src/main/java/com/atrainingtracker/banalservice/ui/devices/devicelist/ListDeviceViewModel.kt
+++ app/src/main/java/com/atrainingtracker/banalservice/ui/devices/devicelist/ListDeviceViewModel.kt
@@ -4,8 +4,6 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.atrainingtracker.banalservice.Protocol
 import com.atrainingtracker.banalservice.devices.DeviceType
 import com.atrainingtracker.banalservice.helpers.BatteryStatusHelper
