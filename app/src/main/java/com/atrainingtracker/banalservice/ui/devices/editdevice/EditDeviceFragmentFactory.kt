77396628b83d9a7046318d31356c4c675103dd53
Rainer Blind <rainer.blind@gmail.com>
2026-02-20 17:57:59 +0100

    Feature: Modify the MainActivity to use the newly created EditDeviceFragments.

--- app/src/main/java/com/atrainingtracker/banalservice/ui/devices/editdevice/EditDeviceFragmentFactory.kt
+++ app/src/main/java/com/atrainingtracker/banalservice/ui/devices/editdevice/EditDeviceFragmentFactory.kt
@@ -7,24 +7,27 @@ import com.atrainingtracker.banalservice.devices.DeviceType
  * A factory object responsible for creating the correct instance of an
  * edit device dialog based on the device's type.
  */
-object EditDeviceFragmentFactory {
+class EditDeviceFragmentFactory private constructor() {
 
-    /**
-     * Creates and returns the appropriate DialogFragment for editing a device.
-     * @param deviceId The ID of the device to edit.
-     * @param deviceType The type of the device.
-     * @return A DialogFragment instance ready to be shown.
-     */
-    fun create(deviceId: Long, deviceType: DeviceType): DialogFragment {
-        return when (deviceType) {
+    companion object  {
+        /**
+         * Creates and returns the appropriate DialogFragment for editing a device.
+         * @param deviceId The ID of the device to edit.
+         * @param deviceType The type of the device.
+         * @return A DialogFragment instance ready to be shown.
+         */
+        @JvmStatic
+        fun create(deviceId: Long, deviceType: DeviceType): DialogFragment {
+            return when (deviceType) {
 
-            DeviceType.RUN_SPEED -> EditRunDeviceFragment.newInstance(deviceId)
-            DeviceType.BIKE_SPEED,
-            DeviceType.BIKE_SPEED_AND_CADENCE -> EditSimpleBikeDeviceFragment.newInstance(deviceId)
-            DeviceType.BIKE_POWER -> EditBikePowerDeviceFragment.newInstance(deviceId)
-            DeviceType.BIKE_CADENCE -> EditGeneralDeviceFragment.newInstance(deviceId)
+                DeviceType.RUN_SPEED -> EditRunDeviceFragment.newInstance(deviceId)
+                DeviceType.BIKE_SPEED,
+                DeviceType.BIKE_SPEED_AND_CADENCE -> EditSimpleBikeDeviceFragment.newInstance(deviceId)
+                DeviceType.BIKE_POWER -> EditBikePowerDeviceFragment.newInstance(deviceId)
+                DeviceType.BIKE_CADENCE -> EditGeneralDeviceFragment.newInstance(deviceId)
 
-            else -> EditGeneralDeviceFragment.newInstance(deviceId)
+                else -> EditGeneralDeviceFragment.newInstance(deviceId)
+            }
         }
     }
 }
