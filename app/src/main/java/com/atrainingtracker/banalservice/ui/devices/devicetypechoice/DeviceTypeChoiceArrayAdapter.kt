9f88b308d0d9f07ab6d46b7840e7fddd31f8448b
Rainer Blind <rainer.blind@gmail.com>
2026-02-23 19:26:53 +0100

    Refactor: Migrate the DeviceTypeChoiceFragment and its corresponding Adapter to Kotlin.

---
+++ app/src/main/java/com/atrainingtracker/banalservice/ui/devices/devicetypechoice/DeviceTypeChoiceArrayAdapter.kt
@@ -0,0 +1,39 @@
+package com.atrainingtracker.banalservice.ui.devices.devicetypechoice
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.semantics.text
+
+import com.atrainingtracker.R
+import com.atrainingtracker.banalservice.Protocol
+import com.atrainingtracker.banalservice.devices.DeviceType
+import com.atrainingtracker.banalservice.helpers.UIHelper
+
+class DeviceTypeChoiceArrayAdapter(
+    context: Context,
+    private val deviceTypes: List<DeviceType>,
+    private val protocol: Protocol
+) : ArrayAdapter<DeviceType>(context, R.layout.device_choice_row, deviceTypes) {
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val rowView = convertView ?: inflater.inflate(R.layout.device_choice_row, parent, false)
+
+        val deviceType = deviceTypes[position]
+
+        val textView = rowView.findViewById<TextView>(R.id.title)
+        val iconView = rowView.findViewById<ImageView>(R.id.icon)
+
+        textView.setText(UIHelper.getNameId(deviceType))
+        iconView.setImageResource(UIHelper.getIconId(deviceType, protocol))
+
+        return rowView
+    }
+}
