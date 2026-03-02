3d68aa99dde3363393117020ba8b3366b177beee
Rainer Blind <rainer.blind@gmail.com>
2026-03-02 18:50:22 +0100

    Feature: Make the ColAdder as high as possible.

--- app/src/main/java/com/atrainingtracker/trainingtracker/ui/tracking/tracking/SensorGridScreen.kt
+++ app/src/main/java/com/atrainingtracker/trainingtracker/ui/tracking/tracking/SensorGridScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -163,7 +164,7 @@ private fun RowAdder(onClick: () -> Unit) {
 
 @Composable
 private fun ColAdder(onClick: () -> Unit) {
-    IconButton(onClick = onClick) {
+    IconButton(onClick = onClick, modifier = Modifier.fillMaxHeight()) {
         Icon(
             imageVector = Icons.Default.Add,
             contentDescription = "Add Column",
