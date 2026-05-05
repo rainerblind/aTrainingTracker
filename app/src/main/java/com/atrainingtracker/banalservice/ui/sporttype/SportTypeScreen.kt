0613e4b9e5e9b8ea40d0bcfb78455ef9700e022b
Rainer Blind <rainer.blind@gmail.com>
2026-05-05 07:52:53 +0200

    Improvement: Some more padding for the SportTypeScreen.kt

--- app/src/main/java/com/atrainingtracker/banalservice/ui/sporttype/SportTypeScreen.kt
+++ app/src/main/java/com/atrainingtracker/banalservice/ui/sporttype/SportTypeScreen.kt
@@ -102,7 +102,7 @@ fun SportTypeScreen(
 
     val density = LocalDensity.current
 
-    val appBarMaxHeightPx = with(density) { 125.dp.roundToPx() }
+    val appBarMaxHeightPx = with(density) { 127.dp.roundToPx() }
     val connection = remember(appBarMaxHeightPx) {
         CollapsingAppBarNestedScrollConnection(appBarMaxHeightPx)
     }
