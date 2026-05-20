8749b4ddb5a17427c5e426d28ff116bd0bfb8f41
Rainer Blind <rainer.blind@gmail.com>
2026-05-20 19:57:09 +0200

    Bugfix: Directly set the color of the PrimaryScrollableTabRow to surfaceContainerLow.

--- app/src/main/java/com/atrainingtracker/trainingtracker/ui/aftermath/periodlist/PeriodsTabsScreen.kt
+++ app/src/main/java/com/atrainingtracker/trainingtracker/ui/aftermath/periodlist/PeriodsTabsScreen.kt
@@ -97,7 +97,6 @@ fun PeriodsTabsScreen(
             Surface(
                 modifier = Modifier.offset { IntOffset(0, connection.appBarOffset) },
                 color = MaterialTheme.colorScheme.primaryContainer,
-                tonalElevation = 3.dp
             ) {
                 Column(modifier = Modifier.statusBarsPadding()) {
                     // --- Heading Row with Sort Button ---
@@ -120,7 +119,7 @@ fun PeriodsTabsScreen(
                     }
                     PrimaryScrollableTabRow(
                         selectedTabIndex = pagerState.currentPage,
-                        containerColor = MaterialTheme.colorScheme.surface,
+                        containerColor = MaterialTheme.colorScheme.surfaceContainerLow,
                         divider = {}
                     ) {
                         tabs.forEachIndexed { index, title ->
